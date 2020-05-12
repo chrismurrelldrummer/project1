c77ad26a761537ce23572ab3bed141ea0a4084a4
Chris Murrell <chrismurrelldrummer@gmail.com>
2020-05-12 14:23:54 +0100

    under construction paths set

--- application.py
+++ application.py
@@ -27,8 +27,58 @@ db = scoped_session(sessionmaker(bind=engine))
 @app.route("/")
 def index():
 
+    return render_template('index.html')
+
+
+@app.route("/search")
+def search():
+
     # Access test goodreads API
     res = requests.get("https://www.goodreads.com/book/review_counts.json",
                        params={"key": "q6gj5umJdwuDCz5OX61pwg", "isbns": "9781632168146"})
+    return render_template('search.html')
 
-    return render_template("index.html", res=res.json())
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                          username=request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return render_template('error', err="Invalid username and/or password", code=403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+        session["username"] = rows[0]["username"]
+
+        # Redirect user to search page
+        return redirect("/search")
+
+    # User reached route via GET
+    else:
+        return render_template("login.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register new user"""
+
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Redirect user to login page
+        return redirect("/login")
+
+    # User reached route via GET
+    else:
+        return render_template("register.html")
