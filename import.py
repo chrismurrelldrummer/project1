c1d64cebc9923b8a9a5bfcf6e8c5442b90520061
Chris Murrell <chrismurrelldrummer@gmail.com>
2020-05-13 15:38:13 +0100

    import completed

--- import.py
+++ import.py
@@ -12,18 +12,32 @@ if not os.getenv("DATABASE_URL"):
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-# import books from CSV file into database
+# set up database tables
+db.execute(CREATE TABLE books(id SERIAL PRIMARY KEY, isbn VARCHAR NOT NULL, title VARCHAR NOT NULL, author VARCHAR NOT NULL, year INTEGER NOT NULL))
+
+db.execute(CREATE TABLE users(id SERIAL PRIMARY KEY, firstname VARCHAR NOT NULL, surname VARCHAR NOT NULL, username VARCHAR UNIQUE NOT NULL, hash TEXT NOT NULL))
+
+db.execute(CREATE TABLE reviews(id SERIAL PRIMARY KEY, userID INTEGER NOT NULL REFERENCES users, bookID INTEGER NOT NULL REFERENCES books, rating INTEGER NOT NULL CHECK(rating <= 5 AND rating >= 1), comment TEXT))
+
+# create file variable
 fname = "books.csv"
 
-# read csv data
+# read and insert csv data
 with open(fname, 'r') as data:
-    read = csv.DictReader(data)
+    read = csv.reader(data)
 
-    for row in read:
-        isbn = row['isbn']
-        title = row['title']
-        author = row['author']
-        year = row['year']
+    for isbn, title, author, year in read:
 
-        db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :ti, :auth, :y)",
-                   isbn=isbn, title=title, auth=author, y=year)
+        if isbn == 'isbn':
+            continue
+        else:
+            db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
+                       {"isbn": isbn, "title": title, "author": author, "year": year})
+
+    db.commit()
+
+print('Import Successful!')
+
+# Query database for import check of first item
+book = db.execute("SELECT * FROM books").fetchone()
+print(book)
